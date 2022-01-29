a5c6ed3943a56d4487a68992cbce335682ba2cad
hiroki6207 <hiroki6207@gmail.com>
2022-01-29 19:54:30 +0900

    ドアダッシュの吹き飛ぶドアの実装 Dotweenの追加

---
+++ Assets/Scripts/WallHelth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHelth : MonoBehaviour
+{
+    [SerializeField] private Wallcrash[] walls;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        int index = Random.Range(0, walls.Length);
+        walls[index].AttachRigidbody();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Wallcrash.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallcrash : MonoBehaviour
+{
+    [SerializeField] private float thrust = 1.0f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        
+    }
+
+    public void AttachRigidbody()
+    {
+        gameObject.AddComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && rb != null)
+        {
+            rb.AddForce(transform.forward * thrust, ForceMode.Impulse);
+            StartCoroutine(DelayDestroy());
+        }
+    }
+
+    public IEnumerator DelayDestroy()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
+    }
+}
