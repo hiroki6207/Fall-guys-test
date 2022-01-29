e1d111b00a70c6b410d88702b335f5203ce86c15
hiroki6207 <hiroki6207@gmail.com>
2022-01-29 20:54:52 +0900

    FadeInOut作成

---
+++ Assets/Scripts/FadeInOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeInOut : MonoBehaviour
+{
+    [SerializeField] private Image background_image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.background_image.DOFade(endValue: 0f, duration: 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
