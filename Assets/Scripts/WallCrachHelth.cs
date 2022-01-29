604163c6f104bf7f7f7277058c9c70640b93e7a6
ktgitfrog333 <paraysis+git@gmail.com>
2022-01-29 14:09:49 +0900

    壊れる壁の実装 スクリプト2つとプレハブ2つ追加

---
+++ Assets/Scripts/WallCrachHelth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCrachHelth : MonoBehaviour
+{
+    [SerializeField] Wallcrash[] walls;
+
+    void Start()
+    {
+        var length = walls.Length;
+        // 壁の一覧を取得して、壊す対象をランダムで一つ決める
+        var idx = Random.Range(0, length);
+        Debug.Log("破壊対象の番号:[" + idx + "]");
+        
+        // 壊す壁の対象に対して、壊す設定を適用する
+        for (var i = 0; i < length; i++)
+        {
+            if (idx == i)
+            {
+                walls[i].AttachCrashMode();
+                // 一つ決めるだけなので以降の処理はスキップする
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Wallcrash.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallcrash : MonoBehaviour
+{
+    [SerializeField] private float thrust = 1.0f;
+    private Rigidbody rb;
+
+    public void AttachCrashMode()
+    {
+        gameObject.AddComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && rb)
+        {
+            rb.AddForce(transform.forward * thrust);
+            StartCoroutine(DelayDestroy());
+        }
+    }
+
+    private IEnumerator DelayDestroy()
+    {
+        // 3秒後に消える
+        yield return new WaitForSeconds(1.5f);
+        Destroy(gameObject);
+    }
+}
