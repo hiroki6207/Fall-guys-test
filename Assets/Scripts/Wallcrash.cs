5e4f43cdaf664549b6966d7de032727a61e6fca4
hiroki6207 <hiroki6207@gmail.com>
2022-01-30 15:40:24 +0900

    パーティクル実装

--- Assets/Scripts/Wallcrash.cs
+++ Assets/Scripts/Wallcrash.cs
@@ -6,6 +6,7 @@ public class Wallcrash : MonoBehaviour
 {
     [SerializeField] private float thrust = 1.0f;
     public Rigidbody rb;
+    public Transform prefab;
     // Start is called before the first frame update
     public void Start()
     {
@@ -22,6 +23,12 @@ public class Wallcrash : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Player") && rb != null)
         {
+            Vector3 hitPos = new Vector3(0, 0, 0);
+            foreach (ContactPoint point in collision.contacts)
+            {
+                hitPos = point.point;
+            }
+            Instantiate(prefab, hitPos, Quaternion.identity);
             rb.AddForce(transform.forward * thrust, ForceMode.Impulse);
             StartCoroutine(DelayDestroy());
         }
