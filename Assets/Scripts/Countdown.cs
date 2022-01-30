ed8cd33af03d6e5b62f0e41d103120a2c2bf4f53
hiroki6207 <hiroki6207@gmail.com>
2022-01-30 14:46:58 +0900

    カウントダウン実装

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] public float countdown = 5.0f;
+    public Text timeText;
+    private bool isPose = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(isPose)
+            {
+                isPose = false;
+            }else
+            {
+                isPose = true;
+            }
+        }
+        
+        if(isPose)
+        {
+            timeText.text = "ポーズ中";
+            return;
+        }
+        countdown -= Time.deltaTime;
+        timeText.text = countdown.ToString("f1") + "秒";
+        if(countdown <= 0)
+        {
+            timeText.text = "時間になりました";
+        }
+    }
+}
