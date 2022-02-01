03cc59b966809103e159fd9c81fb68299b81d990
hiroki6207 <hiroki6207@gmail.com>
2022-02-01 23:47:00 +0900

    timeOutedフラグ実装

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -10,6 +10,7 @@ public class Countdown : MonoBehaviour
     [SerializeField] public float countdown = 5.0f;
     public Text timeText;
     private bool isPose = false;
+    private bool timeOuted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,9 +40,13 @@ public class Countdown : MonoBehaviour
         timeText.text = countdown.ToString("f1") + "秒";
         if(countdown <= 0)
         {
-            GameObject.Find("FadeInOutPanel").GetComponent<FadeInOut>().Fadeout();
-            await Task.Delay(3000);
-            SceneManager.LoadScene("GameOverScene");
+            if(timeOuted == false)
+            {
+                timeOuted = true;
+                GameObject.Find("FadeInOutPanel").GetComponent<FadeInOut>().Fadeout();
+                await Task.Delay(3000);
+                SceneManager.LoadScene("GameOverScene");
+            }
         }
     }
 }
